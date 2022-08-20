b36c2997f3a21086145a06fc41f28c99fcb740c7
Robert Isaac (c) <irobert@vmware.com>
2022-08-20 14:28:04 +0300

    add todo resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodoModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/todo/todo.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+
+@Injectable()
+export class TodoService {
+  create(createTodoDto: CreateTodoDto) {
+    return 'This action adds a new todo';
+  }
+
+  findAll() {
+    return `This action returns all todo`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} todo`;
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto) {
+    return `This action updates a #${id} todo`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
