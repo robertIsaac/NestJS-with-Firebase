8b9a214f2a2c2a3abfc46a01fb0ebc431ff4bf7d
Robert Isaac (c) <irobert@vmware.com>
2022-08-20 14:24:48 +0300

    add Firebase functions

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import { AppModule } from './src/app.module';
+
+const expressServer = express();
+const createFunction = async (expressInstance): Promise<void> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+};
+export const api = functions.https.onRequest(async (request, response) => {
+  await createFunction(expressServer);
+  expressServer(request, response);
+});
